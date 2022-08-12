6ae4a8130db3e8114a11b082270e9f2153159a15
Lee Inho <inhoking@kookmin.ac.kr>
2022-08-12 21:31:30 +0900

    [full] chore. fix css

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,12 +14,19 @@ export default {
 </script>
 
 <style>
-.haman {
+/* .haman {
   position: absolute;
   top: 25%;
   left: 20%;
   width: 400px;
   height: 100px;
   margin: -50px 0 0 -50px;
+} */
+.haman {
+  position: absolute;
+  top: 15%;
+  left: 25%;
+  width: 50%;
+  height: 20%;
 }
 </style>
